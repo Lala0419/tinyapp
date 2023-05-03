75cffa31acf3c8975037db3e133a8490d61a6809
Nanaya Miki <nanayamiki@gmail.com>
2023-05-03 17:05:45 +0000

    added key-pair values for reading cookies to other routes

--- express_server.js
+++ express_server.js
@@ -47,13 +47,19 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-	res.render("urls_new");
+	const templateVars = {
+		username: req.cookies["username"],
+		urls: urlDatabase,
+	};
+	res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
 	const templateVars = {
 		id: req.params.id,
 		longURL: urlDatabase[req.params.id],
+		username: req.cookies["username"],
+		urls: urlDatabase,
 	};
 	res.render("urls_show", templateVars);
 });
